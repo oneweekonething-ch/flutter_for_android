4c8a901fde7c6af46f1ca5c3756e684baaed2c5a
pandabo1985 <pandabo1985@gmail.com>
2021-12-05 19:16:00 +0800

    refs 如何布局？ XML layout 文件跑哪去了？

--- lib/main.dart
+++ lib/main.dart
@@ -19,32 +19,51 @@ class SampleAppPage extends StatefulWidget {
   SampleAppPage({Key? key}) : super(key: key);
 
   @override
-  _SampleAppPageState createState() {
-    return new _SampleAppPageState();
+  _XmlAppPageState createState() {
+    return new _XmlAppPageState();
   }
 }
 
-class _SampleAppPageState extends State<SampleAppPage> {
-  String textToShow = "I like show flutter";
-  int updateNum = 0;
-
-  void _updateText() {
-    setState(() {
-      updateNum += 1;
-      textToShow = "Flutter is fuck Awesome $updateNum";
-    });
-  }
-
+class _XmlAppPageState extends State {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Sample app")),
-      body: new Center(child: new Text(textToShow)),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _updateText,
-        tooltip: 'updateText',
-        child: new Icon(Icons.update),
+      appBar: new AppBar(title: new Text("Xml app")),
+      body: new Center(
+        child: new MaterialButton(
+          color: Colors.blue,
+          textColor: Colors.white,
+          onPressed: () {},
+          child: new Text("hello xml"),
+          padding: new EdgeInsets.only(left: 10.0, right: 10.0),
+        ),
       ),
     );
   }
 }
+
+//
+// class _SampleAppPageState extends State<SampleAppPage> {
+//   String textToShow = "I like show flutter";
+//   int updateNum = 0;
+//
+//   void _updateText() {
+//     setState(() {
+//       updateNum += 1;
+//       textToShow = "Flutter is fuck Awesome $updateNum";
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       appBar: new AppBar(title: new Text("Sample app")),
+//       body: new Center(child: new Text(textToShow)),
+//       floatingActionButton: new FloatingActionButton(
+//         onPressed: _updateText,
+//         tooltip: 'updateText',
+//         child: new Icon(Icons.update),
+//       ),
+//     );
+//   }
+// }
